b9063b2278cae2533452ab9b4e92154e8d6b375c
mtefagh <mtefagh@stanford.edu>
2019-01-23 23:01:05 -0800

    version 0.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,5 @@
-include("../sparseQFCA.jl")
-using .sparseQFCA, SparseArrays, DelimitedFiles, Test
-S = sparse(readdlm("../example/S.csv", header = false))
-@assert typeof(S) == SparseMatrixCSC{Float64,Int64}
-rev = readdlm("../example/rev.csv", header = false)[:, 1] .== 1
-@assert typeof(rev) == BitArray{1}
-certificates, blocked, fctable = @time QFCA(S, rev)
-@test all(readdlm("../example/fctable.csv", header = false) .== fctable)
+include("../src/sparseQFCA.jl")
+include("../example/ecoli.jl")
+using .sparseQFCA, .ecoli, Test
+fctable = QFCA(S, rev)[end]
+@test fctest(fctable)
