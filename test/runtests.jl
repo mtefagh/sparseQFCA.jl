bb2fc2933e97b12364e5a84e12cf9fb2fe5a573a
Mojtaba Tefagh <mtefagh@stanford.edu>
2019-02-27 06:37:03 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@
 include("../example/ecoli.jl")
 using .ecoli, Pkg, Test
 Pkg.add("sparseQFCA")
+using sparseQFCA
 # finding all the flux coupling relations among the reactions
 fctable = @time QFCA(S, rev)[end]
 @test fctest(fctable)
