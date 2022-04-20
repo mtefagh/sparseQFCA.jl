f9594bb9212f95c3e02c1dd20f4731ad9d02d309
iMan Ghadimi <iman.diligent@gmail.com>
2022-04-20 15:33:58 +0430

    Update find_blocked_runTests.jl

--- test/find_blocked_runTests.jl
+++ test/find_blocked_runTests.jl
@@ -5,17 +5,17 @@ using .find_blocked_data, TheNaiveApproach, Test
 
 # finding all blocked reactions among the reactions
 
-blockedList = @time find_blocked_reactions(myModel_e_coli_core)[end]
-@test blockedTest(blockedList)
+blockedList_e_coli_core = @time find_blocked_reactions(myModel_e_coli_core)[end]
+@test blockedTest_e_coli_core(blockedList_e_coli_core)
 
-blockedList = @time find_blocked_reactions(myModel_iAB_RBC_283)[end]
-@test blockedTest(blockedList)
+blockedList_iAB_RBC_283 = @time find_blocked_reactions(myModel_iAB_RBC_283)[end]
+@test blockedTest_iAB_RBC_283(blockedList_iAB_RBC_283)
 
-blockedList = @time find_blocked_reactions(myModel_iAF692)[end]
-@test blockedTest(blockedList)
+blockedList_iAF692 = @time find_blocked_reactions(myModel_iAF692)[end]
+@test blockedTest(blockedList_iAF692)
 
-blockedList = @time find_blocked_reactions(myModel_iCN900)[end]
-@test blockedTest(blockedList)
+blockedList_iCN900 = @time find_blocked_reactions(myModel_iCN900)[end]
+@test blockedTest(blockedList_iCN900)
 
-blockedList = @time find_blocked_reactions(myModel_Recon3D)[end]
-@test blockedTest(blockedList)
+blockedList_Recon3D = @time find_blocked_reactions(myModel_Recon3D)[end]
+@test blockedTest(blockedList_Recon3D)
