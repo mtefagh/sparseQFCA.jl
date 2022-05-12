e77b7a01048b10d8f492c7b59d8c7daafdcad8bc
iMan Ghadimi <iman.diligent@gmail.com>
2022-05-12 15:16:59 +0430

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,15 +19,15 @@ using .find_blocked_data, .pre_processing, .TheNaiveApproach
 blockedList_ecoli_core = @time find_blocked_reactions(myModel_ecoli_core)
 @test blockedTest_ecoli_core(blockedList_ecoli_core)
 
-blockedList_iAB_RBC_283 = @time find_blocked_reactions(myModel_iAB_RBC_283)[end]
+blockedList_iAB_RBC_283 = @time find_blocked_reactions(myModel_iAB_RBC_283)
 @test blockedTest_iAB_RBC_283(blockedList_iAB_RBC_283)
 
-blockedList_iAF692 = @time find_blocked_reactions(myModel_iAF692)[end]
+blockedList_iAF692 = @time find_blocked_reactions(myModel_iAF692)
 @test blockedTest_iAF692(blockedList_iAF692)
 
-blockedList_iCN900 = @time find_blocked_reactions(myModel_iCN900)[end]
+blockedList_iCN900 = @time find_blocked_reactions(myModel_iCN900)
 @test blockedTest_iCN900(blockedList_iCN900)
 
-blockedList_Recon3D = @time find_blocked_reactions(myModel_Recon3D)[end]
+blockedList_Recon3D = @time find_blocked_reactions(myModel_Recon3D)
 @test blockedTest_Recon3D(blockedList_Recon3D)
 
