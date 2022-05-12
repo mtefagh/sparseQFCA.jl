af02bc05d3283e833904ca093432d64c411d07bb
iMan Ghadimi <iman.diligent@gmail.com>
2022-05-12 12:56:02 +0430

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,10 +23,11 @@ blockedList_iAB_RBC_283 = @time find_blocked_reactions(myModel_iAB_RBC_283)[end]
 @test blockedTest_iAB_RBC_283(blockedList_iAB_RBC_283)
 
 blockedList_iAF692 = @time find_blocked_reactions(myModel_iAF692)[end]
-@test blockedTest(blockedList_iAF692)
+@test blockedTest_iAF692(blockedList_iAF692)
 
 blockedList_iCN900 = @time find_blocked_reactions(myModel_iCN900)[end]
-@test blockedTest(blockedList_iCN900)
+@test blockedTest_iCN900(blockedList_iCN900)
 
 blockedList_Recon3D = @time find_blocked_reactions(myModel_Recon3D)[end]
-@test blockedTest(blockedList_Recon3D)
+@test blockedTest_Recon3D(blockedList_Recon3D)
+
