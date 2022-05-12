f46765ef7b13309dd4d3d08290cd6f5f44625373
iMan Ghadimi <iman.diligent@gmail.com>
2022-05-12 12:37:05 +0430

    Update find_blocked_data.jl

--- example/find_blocked_data.jl
+++ example/find_blocked_data.jl
@@ -2,6 +2,7 @@ module find_blocked_data
 
 export myModel_ecoli_core, myModel_iAB_RBC_283, myModel_iAF692, myModel_iCN900, myModel_Recon3D,
        blocked_ecoli_core, blocked_iAB_RBC_283, blocked_iAF692, blocked_iCN900, blocked_Recon3D,
+       blockedTest_ecoli_core, blockedTest_iAB_RBC_283, blockedTest_iAF692, blockedTest_iCN900, blockedTest_Recon3D
        
 
 using COBREXA, DelimitedFiles
