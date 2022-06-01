1d25ab9516d8b682ad2709edbe593a87636a32ee
iMan Ghadimi <iman.diligent@gmail.com>
2022-06-01 17:03:43 +0430

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,13 +20,13 @@ blockedList_ecoli_core = @time find_blocked_reactions(myModel_ecoli_core)
 @test blockedTest_ecoli_core(blockedList_ecoli_core)
 
 blockedList_iAB_RBC_283 = @time find_blocked_reactions(myModel_iAB_RBC_283)
-#@test blockedTest_iAB_RBC_283(blockedList_iAB_RBC_283)
+@test blockedTest_iAB_RBC_283(blockedList_iAB_RBC_283)
 
-blockedList_iAF692 = @time find_blocked_reactions(myModel_iAF692)
+#blockedList_iAF692 = @time find_blocked_reactions(myModel_iAF692)
 #@test blockedTest_iAF692(blockedList_iAF692)
 
-blockedList_iCN900 = @time find_blocked_reactions(myModel_iCN900)
+#blockedList_iCN900 = @time find_blocked_reactions(myModel_iCN900)
 #@test blockedTest_iCN900(blockedList_iCN900)
 
-blockedList_Recon3D = @time find_blocked_reactions(myModel_Recon3D)
+#blockedList_Recon3D = @time find_blocked_reactions(myModel_Recon3D)
 #@test blockedTest_Recon3D(blockedList_Recon3D)
