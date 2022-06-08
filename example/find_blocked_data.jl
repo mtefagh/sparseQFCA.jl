e0659f96ce7a67120a6d71b57ca162634eeffa38
iMan Ghadimi <iman.diligent@gmail.com>
2022-06-08 15:25:01 +0430

    Update find_blocked_data.jl

--- example/find_blocked_data.jl
+++ example/find_blocked_data.jl
@@ -9,8 +9,8 @@ using COBREXA, DelimitedFiles
 myModel_ecoli_core = load_model(StandardModel,"../example/Models/e_coli_core.xml")
 myModel_iAB_RBC_283 = load_model(StandardModel,"../example/Models/iAB_RBC_283.xml")
 myModel_iAF692 = load_model(StandardModel,"../example/Models/iAF692.xml")
-#myModel_iCN900 = load_model(StandardModel,"../example/Models/iCN900.xml")
-#myModel_Recon3D = load_model(StandardModel,"../example/Models/Recon3D.xml")
+myModel_iCN900 = load_model(StandardModel,"../example/Models/iCN900.xml")
+myModel_Recon3D = load_model(StandardModel,"../example/Models/Recon3D.xml")
 
 blocked_ecoli_core = readdlm("../example/PythonResults/e_coli_core.csv", header = false)
 blocked_iAB_RBC_283 = readdlm("../example/PythonResults/iAB_RBC_283.csv", header = false)
