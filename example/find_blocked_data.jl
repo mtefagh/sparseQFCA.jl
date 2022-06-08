c51166b85786c6c393892ed39ffa2ef316fe3003
iMan Ghadimi <iman.diligent@gmail.com>
2022-06-08 15:19:29 +0430

    Update find_blocked_data.jl

--- example/find_blocked_data.jl
+++ example/find_blocked_data.jl
@@ -15,17 +15,17 @@ myModel_iAF692 = load_model(StandardModel,"../example/Models/iAF692.xml")
 blocked_ecoli_core = readdlm("../example/PythonResults/e_coli_core.csv", header = false)
 blocked_iAB_RBC_283 = readdlm("../example/PythonResults/iAB_RBC_283.csv", header = false)
 blocked_iAF692 = readdlm("../example/PythonResults/iAF692.csv", header = false)
-#blocked_iCN900 = readdlm("../example/PythonResults/iCN900.csv", header = false)
-#try
-#       blocked_Recon3D = readdlm("../example/PythonResults/Recon3D.csv", header = false)
-#catch e
-#       blocked_Recon3D = Array{Any}(undef, 0, 2)
-#end
+blocked_iCN900 = readdlm("../example/PythonResults/iCN900.csv", header = false)
+try
+       blocked_Recon3D = readdlm("../example/PythonResults/Recon3D.csv", header = false)
+catch e
+       blocked_Recon3D = Array{Any}(undef, 0, 2)
+end
 
 blockedTest_ecoli_core(list) = all(list .== blocked_ecoli_core)
 blockedTest_iAB_RBC_283(list) = all(list .== blocked_iAB_RBC_283)
 blockedTest_iAF692(list) = all(list .== blocked_iAF692)
-#blockedTest_iCN900(list) = all(list .== blocked_iCN900)
-#blockedTest_Recon3D(list) = all(list .== blocked_Recon3D)
+blockedTest_iCN900(list) = all(list .== blocked_iCN900)
+blockedTest_Recon3D(list) = all(list .== blocked_Recon3D)
 
 end
