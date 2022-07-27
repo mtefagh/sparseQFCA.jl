8ab489731e3a3164bb30bdda9960a135f3a8c20d
iManGHD <iman.diligent@gmail.com>
2022-07-27 13:54:04 +0430

    Updating runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,29 +22,29 @@ using .find_blocked_data, .pre_processing, .TheNaiveApproach, .SwiftCC
 # iAM_Pv461
 
 blockedList_TheNaive_iAM_Pv461 = @time find_blocked_reactions(myModel_iAM_Pv461)
-blockedList_swiftCC_iAM_Pv461 = @time swifCC(myModel_iAM_Pv461)
+blockedList_swiftCC_iAM_Pv461 = @time swiftCC(myModel_iAM_Pv461)
 @test blockedTest_iAM_Pv461(blockedList_TheNaive_iAM_Pv461, blockedList_swiftCC_iAM_Pv461)
 
 # iAT_PLT_636
 
 blockedList_TheNaive_iAT_PLT_636 = @time find_blocked_reactions(myModel_iAT_PLT_636)
-blockedList_swiftCC_iAT_PLT_636 = @time swifCC(myModel_iAT_PLT_636)
+blockedList_swiftCC_iAT_PLT_636 = @time swiftCC(myModel_iAT_PLT_636)
 @test blockedTest_iAT_PLT_636(blockedList_TheNaive_iAT_PLT_636, blockedList_swiftCC_iAT_PLT_636)
 
 # iCN900
 
 blockedList_TheNaive_iCN900 = @time find_blocked_reactions(myModel_iCN900)
-blockedList_swiftCC_iCN900 = @time swifCC(myModel_iCN900)
+blockedList_swiftCC_iCN900 = @time swiftCC(myModel_iCN900)
 @test blockedTest_iCN900(blockedList_TheNaive_iCN900, blockedList_swiftCC_iCN900)
 
 # iNF517
 
 blockedList_TheNaive_iNF517 = @time find_blocked_reactions(myModel_iNF517)
-blockedList_swiftCC_iNF517 = @time swifCC(myModel_iNF517)
+blockedList_swiftCC_iNF517 = @time swiftCC(myModel_iNF517)
 @test blockedTest_iNF517(blockedList_TheNaive_iNF517, blockedList_swiftCC_iNF517)
 
 # iNJ661
 
 blockedList_TheNaive_iNJ661 = @time find_blocked_reactions(myModel_iNJ661)
-blockedList_swiftCC_iNJ661 = @time swifCC(myModel_iNJ661)
+blockedList_swiftCC_iNJ661 = @time swiftCC(myModel_iNJ661)
 @test blockedTest_iNJ661(blockedList_TheNaive_iNJ661, blockedList_swiftCC_iNJ661)
