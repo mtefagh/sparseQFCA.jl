490234deca24875f9773e1212a8292e2236d7c1d
Mojtaba Tefagh <mtefagh@stanford.edu>
2022-07-29 13:42:31 +0430

    v1.2.0
    version 1.2.0

--- test/ecoli.jl
+++ test/ecoli.jl
@@ -3,13 +3,13 @@ export S, rev, fctest, fcfig
 
 using SparseArrays, DelimitedFiles, Colors
 
-S = sparse(readdlm("../example/S.csv", header = false))
+S = sparse(readdlm("S.csv", header = false))
 @assert typeof(S) == SparseMatrixCSC{Float64,Int64}
 
-rev = readdlm("../example/rev.csv", header = false)[:, 1] .== 1
+rev = readdlm("rev.csv", header = false)[:, 1] .== 1
 @assert typeof(rev) == BitArray{1}
 
-fctable = readdlm("../example/fctable.csv", header = false)
+fctable = readdlm("fctable.csv", header = false)
 fctest(table) = all(table .== fctable)
 
 palette = distinguishable_colors(5)
---
+++ test/find_blocked_data.jl
@@ -0,0 +1,25 @@
+
+module find_blocked_data
+
+export myModel_iAM_Pv461, myModel_iAT_PLT_636, myModel_iCN900, myModel_iNF517, myModel_iNJ661,
+       blockedTest_iAM_Pv461, blockedTest_iAT_PLT_636, blockedTest_iCN900, blockedTest_iNF517, blockedTest_iNJ661
+
+using COBREXA, DelimitedFiles
+
+# Loading Metabolic Networks:
+
+myModel_iAM_Pv461 = load_model(StandardModel,"Models/iAM_Pv461.xml")
+myModel_iAT_PLT_636 = load_model(StandardModel,"Models/iAT_PLT_636.xml")
+myModel_iCN900 = load_model(StandardModel,"Models/iCN900.xml")
+myModel_iNF517 = load_model(StandardModel,"Models/iNF517.xml")
+myModel_iNJ661 = load_model(StandardModel,"Models/iNJ661.xml")
+
+# Defining functions to Compare Results:
+
+blockedTest_iAM_Pv461(list_TheNaive, list_SwiftCC) = all(list_TheNaive .== list_SwiftCC)
+blockedTest_iAT_PLT_636(list_TheNaive, list_SwiftCC) = all(list_TheNaive .== list_SwiftCC)
+blockedTest_iCN900(list_TheNaive, list_SwiftCC) = all(list_TheNaive .== list_SwiftCC)
+blockedTest_iNF517(list_TheNaive, list_SwiftCC) = all(list_TheNaive .== list_SwiftCC)
+blockedTest_iNJ661(list_TheNaive, list_SwiftCC) = all(list_TheNaive .== list_SwiftCC)
+
+end
