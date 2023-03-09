08494afb6dcb93036148b2b31dde3236538aaee7
iManGHD <iman.diligent@gmail.com>
2023-03-09 17:06:32 +0330

    Updating runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,8 @@
 
-# importing the example model and the sparseQFCA module:
+## sparseQFCA:
 
 include("ecoli.jl")
 
-using Distributed
 using .ecoli, COBREXA, JuMP, sparseQFCA, Test
 
 # finding all the flux coupling relations among the reactions:
@@ -11,33 +10,28 @@ using .ecoli, COBREXA, JuMP, sparseQFCA, Test
 fctable = @time QFCA(S, rev)[end]
 @test fctest(fctable)
 
-# importing the example model and the TheNaiveApproach library:
-
-include("TestData.jl")
-include("../src/Data Processing/pre_processing.jl")
-include("../src/Consistency Checking/TheNaiveApproach.jl")
-include("../src/Consistency Checking/SwiftCC.jl")
-using .TestData, .pre_processing, .TheNaiveApproach, .SwiftCC
-
-# Comparing TheNaiveApproach and SwiftCC Outputs:
+## Consistency_Checking:
 
 # 8P
 
 n = 8
 addQFCAProcs(n)
+
+include("TestData.jl")
+include("../src/Data Processing/pre_processing.jl")
+include("../src/Consistency Checking/TheNaiveApproach.jl")
+include("../src/Consistency Checking/SwiftCC.jl")
+include("../src/QFCA/distributedQFCA.jl")
+
+using .TestData, .pre_processing, .TheNaiveApproach, .SwiftCC, .DistributedQFCA
+
+# Comparing TheNaiveApproach and SwiftCC Outputs:
 
 # e_coli_core
 
 blockedList_TheNaive_e_coli_core = @time find_blocked_reactions(myModel_e_coli_core)
 blockedList_swiftCC_e_coli_core = @time swiftCC(myModel_e_coli_core)
 @test blockedTest_e_coli_core(blockedList_TheNaive_e_coli_core, blockedList_swiftCC_e_coli_core)
-
-removeQFCAProcs()
-
-# 8P
-
-n = 8
-addQFCAProcs(n)
 
 # iIS312
 
@@ -47,6 +41,7 @@ blockedList_swiftCC_iIS312 = @time swiftCC(myModel_iIS312)
 
 removeQFCAProcs()
 
+## QFCA:
 ## Comparing final flux coupling table between distributedQFCA and FFCA Algorithms:
 
 # 8P
@@ -57,7 +52,8 @@ addQFCAProcs(n)
 include("TestData.jl")
 include("../src/Data Processing/pre_processing.jl")
 include("../src/Consistency Checking/SwiftCC.jl")
-using .TestData, .pre_processing, .SwiftCC
+include("../src/QFCA/distributedQFCA.jl")
+using .TestData, .pre_processing, .SwiftCC, .DistributedQFCA
 
 fctable_distributedQFCA_e_coli_core = @time distributedQFCA(myModel_e_coli_core)
 fctable_distributedQFCA_iIS312 = @time distributedQFCA(myModel_iIS312)
