b59f6f20def6a2b5de728ee216cb0fd1f788ea2e
iManGHD <iman.diligent@gmail.com>
2023-07-24 16:15:57 +0330

    Update Consistency Checking

---
+++ src/Pre_Processing/Consistency Checking/SwiftCC.jl
@@ -0,0 +1,254 @@
+#-------------------------------------------------------------------------------------------
+#=
+    Purpose:    Identifying blocked reactions in metabolic networks using Convex Optimization(1LP) and Gaussian Elimination
+    Author:     Iman Ghadimi, Mojtaba Tefagh - Sharif University of Technology
+    Date:       July 2022
+=#
+#-------------------------------------------------------------------------------------------
+
+module SwiftCC
+
+export MyModel, myModel_Constructor, swiftCC
+
+using GLPK, JuMP, COBREXA, LinearAlgebra, SparseArrays, Distributed
+
+include("../Pre_processing.jl")
+
+using .Pre_processing
+
+"""
+    MyModel(S, Metabolites, Reactions, Genes, m, n, lb, ub)
+
+A general type for storing a StandardModel which contains the following fields:
+
+- `S`:              LHS matrix (m x n)
+- `Metabolites`:    List of metabolic network metabolites.
+- `Reactions`:      List of metabolic network reactions.
+- `Genes`:          List of metabolic network reactions.
+- `m`:              Number of rows of stoichiometric matrix.
+- `n`:              Number of columns of stoichiometric matrix.
+- `lb`:             Lower bound vector (n x 1)
+- `ub`:             Upper bound vector (n x 1)
+
+"""
+
+mutable struct MyModel
+    S              ::Union{SparseMatrixCSC{Float64,Int64}, AbstractMatrix}
+    Metabolites    ::Array{String,1}
+    Reactions      ::Array{String,1}
+    Genes          ::Array{String,1}
+    m              ::Int
+    n              ::Int
+    lb             ::Array{Float64,1}
+    ub             ::Array{Float64,1}
+end
+
+#-------------------------------------------------------------------------------------------
+
+"""
+    myModel_Constructor(ModelObject, S, Metabolites, Reactions, Genes, m, n, lb, ub)
+
+The function takes in several arguments, including a ModelObject of type MyModel,
+and assigns values to its fields based on the other arguments passed in.
+
+# INPUTS
+
+-'ModelObject':     A newly object of MyModel.
+- `S`:              LHS matrix (m x n)
+- `Metabolites`:    List of metabolic network metabolites.
+- `Reactions`:      List of metabolic network reactions.
+- `Genes`:          List of metabolic network reactions.
+- `m`:              Number of rows of stoichiometric matrix.
+- `n`:              Number of columns of stoichiometric matrix.
+- `lb`:             Lower bound vector (n x 1)
+- `ub`:             Upper bound vector (n x 1)
+
+"""
+
+function myModel_Constructor(ModelObject::MyModel, S::Union{SparseMatrixCSC{Float64,Int64}, AbstractMatrix}, Metabolites::Array{String,1}, Reactions::Array{String,1},
+                                         Genes::Array{String,1}, m::Int, n::Int, lb::Array{Float64,1}, ub::Array{Float64,1})
+     ModelObject.S = S
+     ModelObject.Metabolites = Metabolites
+     ModelObject.Reactions = Reactions
+     ModelObject.Genes = Genes
+     ModelObject.m = m
+     ModelObject.n = n
+     ModelObject.lb = lb
+     ModelObject.ub = ub
+end
+
+#-------------------------------------------------------------------------------------------
+
+"""
+    swiftCC(ModelObject)
+
+The function first exports data from ModelObject and performs some calculations to determine the reversibility of reactions
+and the number of blocked reactions. It then creates an optimization model using the GLPK optimizer to identify irreversible blocked reactions,
+and uses Gaussian elimination to identify blocked reversible reactions.
+
+# INPUTS
+
+- `ModelObject`:        A newly object of MyModel.
+
+# OPTIONAL INPUTS
+
+- `Tolerance`:          A small number that represents the level of error tolerance.
+- `printLevel`:         Verbose level (default: 1). Mute all output with `printLevel = 0`.
+
+# OUTPUTS
+
+- `blocked_index`:      IDs of of blocked reactions.
+- `dualVar`:            Dual variables of a specific constraint.
+
+# EXAMPLES
+
+- Full input/output example
+```julia
+julia> blocked_index, dual_var = swiftCC(ModelObject)
+```
+
+See also: `MyModel`, myModel_Constructor(), `reversibility()`, `homogenization()`
+
+"""
+
+function swiftCC(ModelObject::MyModel, Tolerance::Float64=1e-6, printLevel::Int=1)
+
+    ## Extract relevant information from the input model object
+
+    S = ModelObject.S
+    Metabolites = ModelObject.Metabolites
+    Reactions = ModelObject.Reactions
+    m = ModelObject.m
+    n = ModelObject.n
+    lb = ModelObject.lb
+    ub = ModelObject.ub
+
+    ## Identify which reactions are irreversible and which are reversible
+
+    # Create an array of reaction IDs:
+    Reaction_Ids = collect(1:n)
+    irreversible_reactions_id, reversible_reactions_id = reversibility(lb, Reaction_Ids, 0)
+    n_irr = length(irreversible_reactions_id)
+    n_rev = length(reversible_reactions_id)
+
+    ## Calculate the dimensions of the S matrix
+
+    row_num, col_num = size(S)
+
+    ## Find irreversible blocked reactions in 1 LP problem
+
+    # Set the lower and upper bounds of the "u" variables to 0 and 1, respectively:
+    lb_u = zeros(n_irr)
+    ub_u = ones(n_irr)
+
+    # Create a new optimization model using the GLPK optimizer:
+    model = Model(GLPK.Optimizer)
+
+    # Define variables V and u with their lower and upper bounds:
+    @variable(model, lb[i] <= V[i = 1:n] <= ub[i])
+    @variable(model, lb_u[i] <= u[i = 1:n_irr] <= ub_u[i])
+
+    # Add a constraint that ensures the mass balance of the system:
+    @constraint(model, c1, S * V .== 0)
+
+    # Define the objective function to be the sum of all "u" variables:
+    objective_function = ones(n_irr)'*u
+
+    # Set the optimization objective to maximize the objective function:
+    @objective(model, Max, objective_function)
+
+    # Add a constraint for each irreversible reaction that sets its "u" variable to be less than or equal to its corresponding V variable:
+    @constraint(model, [i in 1:n_irr], u[i] <= V[irreversible_reactions_id[i]])
+
+    # Solve the optimization problem:
+    optimize!(model)
+
+    # Get the dual variables for the stoichiometric constraint:
+    dualVar = dual.(c1)
+
+    # Initialize an empty list to store the IDs of blocked irreversible reactions:
+    irr_blocked_reactions = []
+
+    # Iterate over the irreversible reactions and check if the corresponding u variable is close to 0, If it is, the reaction is considered blocked and its ID is added to the list:
+    for i in range(1,n_irr; step=1)
+        if isapprox(value(u[i]), 0.0, atol = Tolerance)
+            append!(irr_blocked_reactions, irreversible_reactions_id[i])
+        end
+    end
+
+    # Determine the number of irreversible blocked reactions:
+    irr_blocked_num = length(irr_blocked_reactions)
+
+    ## Find reversible blocked reactions
+
+    # Create S_transpose:
+    S_transpose = S'
+
+    # Determine the dimensions of the S_transpose matrix:
+    row_num_trans, col_num_trans = size(S_transpose)
+
+    # Remove irreversibly blocked reactions from the Stoichiometric Matrix:
+    S_transpose_noIrrBlocked = S_transpose[setdiff(1:end, irr_blocked_reactions), :]
+
+    # Create the I_reversible Matrix:
+    I_reversible = zeros(n, n_rev)
+
+    # Populate the I_reversible Matrix with 1 in the rows corresponding to reversible reactions:
+    rev_id = 1
+    for col in eachcol(I_reversible)
+        col[reversible_reactions_id[rev_id]] = 1.0
+        rev_id += 1
+    end
+
+    # Remove irreversibe blocked reactions from the I_reversible Matrix:
+    I_reversible = I_reversible[setdiff(1:end, irr_blocked_reactions), :]
+
+    # Determine the dimensions of the S_transpose_noIrrBlocked and I_reversible matrices:
+    S_trans_row, S_trans_col = size(S_transpose_noIrrBlocked)
+    I_row, I_col = size(I_reversible)
+
+    # Solve the system of equations using Gaussian elimination to identify blocked reversible reactions:
+    X = S_transpose_noIrrBlocked \ I_reversible
+    Sol = (S_transpose_noIrrBlocked*X) - I_reversible
+
+    # Determine the dimensions of the Sol matrix:
+    row_sol, col_sol = size(Sol)
+
+    # Find columns in Sol that correspond to blocked reversible reactions:
+    c = 0
+    rev_blocked_reactions_col = []
+    for col in eachcol(Sol)
+        c += 1
+        if isapprox(norm(col), 0, atol = Tolerance)
+            append!(rev_blocked_reactions_col, c)
+        end
+    end
+
+    rev_blocked_reactions = []
+    for i in rev_blocked_reactions_col
+        append!(rev_blocked_reactions, reversible_reactions_id[i])
+    end
+
+    ## Union reversbile blocked reactions and irreversible blocked reactions
+
+    blocked_index = []
+    blocked_index = union(rev_blocked_reactions, irr_blocked_reactions)
+
+    ## Returning a list consist of the Ids of the blocked reactions
+
+    blocked_index = sort(blocked_index)
+
+    ## Print out results if requested
+
+    if printLevel > 0
+        printstyled("Consistency_Checking(SwiftCC):\n"; color=:cyan)
+        printstyled("Tolerance = $Tolerance\n"; color=:magenta)
+        println("Number of irreversible blocked reactions : $(length(irr_blocked_reactions))")
+        println("Number of reversible   blocked reactions : $(length(rev_blocked_reactions))")
+        println("Number of blocked reactions              : $(length(blocked_index))")
+    end
+
+    return blocked_index, dualVar
+end
+
+end
---
+++ src/Pre_Processing/Consistency Checking/TheNaiveApproach.jl
@@ -0,0 +1,205 @@
+#-------------------------------------------------------------------------------------------
+#=
+    Purpose:    Identifying Blocked Reactions in a Metabolic Model using Convex Optimization(n_i + 2n_r LP)
+    Author:     Iman Ghadimi, Mojtaba Tefagh - Sharif University of Technology
+    Date:       April 2022
+=#
+#-------------------------------------------------------------------------------------------
+
+module TheNaiveApproach
+
+export find_blocked_reactions
+
+using GLPK, JuMP, COBREXA
+
+include("../Pre_processing.jl")
+
+using .Pre_processing
+
+"""
+    find_blocked_reactions(myModel)
+
+The function identifies blocked reactions in a metabolic model. Blocked reactions are reactions that cannot carry any flux,
+i.e., the flux through the reaction is zero, due to the stoichiometry of the model and the constraints on the reaction rates.
+The function first homogenizes the bounds on the reaction rates, and then identifies blocked irreversible reactions by optimizing
+the flux through each reaction while constraining the flux to be less than or equal to 1. If the optimal flux is approximately zero,
+the reaction is considered blocked. Then, the function identifies blocked reversible reactions by optimizing the flux through each
+reaction in both directions, forward and backward, while constraining the flux to be less than or equal to 1 in the forward direction
+and greater than or equal to -1 in the backward direction. If the optimal flux in both directions is approximately zero,the reaction
+is considered blocked. The function returns the IDs of the blocked reactions.
+
+# INPUTS
+
+- `myModel`:            A StandardModel that has been built using COBREXA's `load_model` function.
+
+# OPTIONAL INPUTS
+
+- `Tolerance`:          A small number that represents the level of error tolerance.
+- `printLevel`:         Verbose level (default: 1). Mute all output with `printLevel = 0`.
+
+# OUTPUTS
+
+- `blocked_index`:      IDs of of blocked reactions.
+
+# EXAMPLES
+
+- Full input/output example
+```julia
+julia> blocked_index = find_blocked_reactions(myModel)
+```
+
+See also: `dataOfModel()`, `reversibility()`
+
+"""
+
+function find_blocked_reactions(myModel::StandardModel, Tolerance::Float64=1e-6, printLevel::Int=1)
+
+    ## Export data from model
+
+    S, Metabolites, Reactions, Genes, m, n, lb, ub = dataOfModel(myModel)
+
+    ## Determine the reversibility of a reaction
+
+    # Create an array of reaction IDs:
+    Reaction_Ids = collect(1:n)
+    irreversible_reactions_id, reversible_reactions_id = reversibility(lb, Reaction_Ids)
+    n_irr = length(irreversible_reactions_id)
+    n_rev = length(reversible_reactions_id)
+
+    ## Homogenize the upper_bound and lower_bound of reactions
+
+    printstyled("Homogenization:\n"; color=:cyan)
+
+    # Set the maximum value for M:
+    M = getM()
+
+    ## Loop through each value in the array "lb" and "ub"
+
+    for i in 1:n
+        # If the lower bound is greater than zero, set it to zero:
+        if lb[i] > 0
+            lb[i] = 0
+        end
+        # If the upper bound is greater than zero, set it to M:
+        if ub[i] > 0
+            ub[i] = M
+        end
+        # If the lower bound is less than zero, set it to -M:
+        if lb[i] < 0
+            lb[i] = -M
+        end
+        # If the upper bound is less than zero, set it to zero:
+        if ub[i] < 0
+            ub[i] = 0
+        end
+    end
+
+    ## Find Irreversible blocked reactions
+
+    # Create empty arrays to hold the IDs of blocked and unblocked irreversible reactions:
+    irreversible_blocked_reactions_id = []
+    irreversible_unblocked_reactions_id = []
+
+    # Create a new optimization model using the GLPK optimizer:
+    model_irr = Model(GLPK.Optimizer)
+
+    # Define the variable V for each reaction, with its lower and upper bounds:
+    @variable(model_irr, lb[i] <= V[i = 1:n] <= ub[i])
+
+    # Add a constraint to enforce that S*V = 0:
+    @constraint(model_irr, S * V .== 0)
+
+    ## Loop through each irreversible reaction in the irreversible reactions array
+
+    for j in irreversible_reactions_id
+        # Set the objective to maximize the flux of the current reaction:
+        @objective(model_irr, Max, V[j])
+
+        # Add a constraint to enforce that the flux of the current reaction is less than or equal to 1:
+        @constraint(model_irr, c,  V[j] <= 1)
+
+        # Optimize the model:
+        optimize!(model_irr)
+
+        # If the objective value is approximately zero, add the current reaction ID to the blocked reactions array:
+        if isapprox(objective_value(model_irr), 0, atol = Tolerance)
+            append!(irreversible_blocked_reactions_id, j)
+        # Otherwise, add the current reaction ID to the unblocked reactions array:
+        else
+            append!(irreversible_unblocked_reactions_id, j)
+        end
+
+        # Remove the current constraint from the model:
+        delete(model_irr, c)
+
+        # Unregister the current constraint from the model:
+        unregister(model_irr, :c)
+    end
+
+    ## Find Reversible blocked reactions
+
+    # Create empty arrays to hold the IDs of blocked and unblocked reversible reactions:
+    reversible_unblocked_reactions_id = []
+    reversible_blocked_reactions_id = []
+
+    # Create a new optimization model using the GLPK optimizer:
+    model_rev = Model(GLPK.Optimizer)
+
+    # Define the variable V for each reaction, with its lower and upper bounds:
+    @variable(model_rev, lb[i] <= V[i = 1:n] <= ub[i])
+
+    # Add a constraint to enforce that S*V = 0:
+    @constraint(model_rev, S * V .== 0)
+
+    # Loop through each reversible reaction in the reversible reactions array:
+    for j in reversible_reactions_id
+
+        ## Forward direction
+
+        @objective(model_rev, Max, V[j]) # Set the objective to maximize the flux of the current reaction
+        @constraint(model_rev, c1, V[j] <= 1) # Add a constraint to enforce that the flux of the current reaction is less than or equal to 1
+        optimize!(model_rev) # Optimize the model
+        opt_fwd = objective_value(model_rev) # Save the objective value
+        t_fwd = termination_status(model_rev) # Save the termination status
+        delete(model_rev, c1) # Remove the current constraint from the model
+        unregister(model_rev, :c1) # Unregister the current constraint from the model
+
+        ## Backward direction
+
+        @objective(model_rev, Min, V[j]) # Set the objective to minimize the flux of the current reaction
+        @constraint(model_rev, c2, V[j] >= -1) # Add a constraint to enforce that the flux of the current reaction is greater than or equal to -1
+        optimize!(model_rev) # Optimize the model
+        opt_back = objective_value(model_rev) # Save the objective value
+        t_back = termination_status(model_rev) # Save the termination status
+        delete(model_rev, c2) # Remove the current constraint from the model
+        unregister(model_rev, :c2) # Unregister the current constraint from the model
+
+        # If the objective values in both directions are approximately zero, add the current reaction ID to the blocked reactions array:
+        if isapprox(opt_fwd, 0, atol = Tolerance) && isapprox(opt_back, 0, atol = Tolerance)
+            append!(reversible_blocked_reactions_id, j)
+        # Otherwise, add the current reaction ID to the unblocked reactions array:
+        else
+            append!(reversible_unblocked_reactions_id, j)
+        end
+    end
+
+    ## Union the IDs of blocked irreversible and reversible reactions into a single array and Sort the array of blocked reaction IDs
+
+    blocked_index = union(reversible_blocked_reactions_id, irreversible_blocked_reactions_id)
+    blocked_index = sort(blocked_index)
+
+    ## Print out results if requested
+
+    if printLevel > 0
+        printstyled("Consistency_Checking(TheNaiveApproch):\n"; color=:cyan)
+        printstyled("Tolerance = $Tolerance\n"; color=:magenta)
+        println("Number of irreversible blocked reactions : $(length(irreversible_blocked_reactions_id))")
+        println("Number of reversible   blocked reactions : $(length(reversible_blocked_reactions_id))")
+        println("Number of blocked reactions              : $(length(blocked_index))")
+    end
+
+    return blocked_index
+
+end
+
+end
