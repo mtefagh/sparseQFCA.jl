b01c2186bd574282e20dc68ab6c4ffe60cdc54c3
Iman Ghadimi <iman.diligent@gmail.com>
2023-07-26 12:52:10 +0330

    Update TestData.jl

--- test/TestData.jl
+++ test/TestData.jl
@@ -7,16 +7,33 @@ export myModel_e_coli_core, myModel_iIS312, fctable_FFCA_e_coli_core, fctable_FF
 
 using COBREXA, DelimitedFiles, SparseArrays, HTTP
 
+
+# Create a directory to save the XML models:
+output_directory = "Models"
+if !isdir(output_directory)
+    mkdir(output_directory)
+end
+
 ## Load Metabolic Networks
 
-# e_coli_core:
+## e_coli_core
+
 e_coli_core_model = HTTP.get("http://bigg.ucsd.edu/static/models/e_coli_core.xml")
-write("Models/e_coli_core_model.xml",e_coli_core_model.body)
+# Save the e_coli_core_model to a file with the corresponding model name:
+model_file_path = joinpath(output_directory, "$e_coli_core.xml")
+open(model_file_path, "w") do file
+       write(file, e_coli_core_model)
+end
 myModel_e_coli_core = load_model(StandardModel,"Models/e_coli_core_model.xml")
 
-# iIS312:
+## iIS312
+
 iIS312_model = HTTP.get("http://bigg.ucsd.edu/static/models/iIS312.xml")
-write("Models/iIS312_model.xml",ecoli_model.body)
+# Save the iIS312_model to a file with the corresponding model name:
+model_file_path = joinpath(output_directory, "$iIS312.xml")
+open(model_file_path, "w") do file
+       write(file, iIS312_model)
+end
 myModel_iIS312 = load_model(StandardModel,"Models/iIS312_model.xml")
 
 ## read the CSV file into a matrix of type String
